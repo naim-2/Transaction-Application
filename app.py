d4c5ac95ba5f8cd748db958e8c2e3051d0f7a655
naim-2 <121217709+naim-2@users.noreply.github.com>
2023-09-10 12:59:01 +0300

    Added search functionality

--- app.py
+++ app.py
@@ -56,7 +56,24 @@ def delete_transaction(transaction_id):
 
     return redirect(url_for("get_transactions"))
 
+# Search Transactions
+@app.route("/search", methods = ['GET', 'POST'])
+def search_transactions():
+    if request.method == 'POST':
+        min = float(request.form['min_amount'])
+        max = float(request.form['max_amount'])
+        filtered_transactions = []
+
+        for transaction in transactions:
+            if transaction['amount'] > min and transaction['amount'] < max:
+                filtered_transactions.append(transaction)
+
+        return render_template("transactions.html", transactions=filtered_transactions)
+
+    else:
+        return render_template("search.html")
+
 # Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
-   
+    
